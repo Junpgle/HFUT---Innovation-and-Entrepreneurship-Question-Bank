f2172df2df1ed91777e9d3783bcb203bac3f8649
Junpgle . <674155783@qq.com>
2026-01-13 22:37:19 +0800

    v3.3.1 多处优化

--- hfut-quiz/src/main.jsx
+++ hfut-quiz/src/main.jsx
@@ -8,7 +8,11 @@ import './index.css' // <--- 必须有这一行，否则 Tailwind 和样式不�
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/report', element: <Report /> },
+  { path: '/report.html', element: <Report /> },
+  { path: '*', element: <div>Not Found</div> },
 ])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
